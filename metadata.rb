b3807ef1d002579f189560aa88fab92c7c55db87
John Bellone <jbellone@bellone.us>
2016-02-18 10:31:16 -0500

    Update the metadata.rb with source/issues url.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'Apache 2.0'
 description 'Wrapper cookbook which configures RHEL systems.'
 long_description 'Wrapper cookbook which configures RHEL systems.'
 version '0.1.0'
+source_url 'https://github.com/johnbellone/redhat-cookbook' if defined?(source_url)
+issues_url 'https://github.com/johnbellone/redhat-cookbook/issues' if defined?(issues_url)
 
 supports 'centos', '>= 5.0'
 supports 'redhat', '>= 5.0'
